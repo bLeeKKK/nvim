67ec63a6665837e8c0a0460ccd0160516d7003ed
李槟 <1961774471@qq.com>
2023-05-26 15:04:38 +0800

    添加注释

--- lua/modules/plugins/editor.lua
+++ lua/modules/plugins/editor.lua
@@ -1,63 +1,76 @@
 local editor = {}
 
+-- 极速jk运动
 editor["rainbowhxch/accelerated-jk.nvim"] = {
 	lazy = true,
 	event = "VeryLazy",
 	config = require("editor.accelerated-jk"),
 }
+-- 自动缓存buffer中的文件
 editor["rmagatti/auto-session"] = {
 	lazy = true,
 	cmd = { "SaveSession", "RestoreSession", "DeleteSession" },
 	config = require("editor.auto-session"),
 }
+-- 自动关闭 引号 括号等
 editor["m4xshen/autoclose.nvim"] = {
 	lazy = true,
 	event = "InsertEnter",
 	config = require("editor.autoclose"),
 }
+-- 更好的退出编辑模式
 editor["max397574/better-escape.nvim"] = {
 	lazy = true,
 	event = { "CursorHold", "CursorHoldI" },
 	config = require("editor.better-escape"),
 }
+-- 当文件过大时自动关闭一些功能
 editor["LunarVim/bigfile.nvim"] = {
 	lazy = false,
 	config = require("editor.bigfile"),
 	cond = require("core.settings").load_big_files_faster,
 }
+-- 优化buffer的控制
 editor["ojroques/nvim-bufdel"] = {
 	lazy = true,
 	event = "BufReadPost",
+	config = require("editor.bufdel"),
 }
+-- 优化使用f键时的体验
 editor["rhysd/clever-f.vim"] = {
 	lazy = true,
 	event = { "BufReadPost", "BufAdd", "BufNewFile" },
 	config = require("editor.cleverf"),
 }
+-- 注释插件（jsx不能注释）
 editor["numToStr/Comment.nvim"] = {
 	lazy = true,
 	event = { "CursorHold", "CursorHoldI" },
 	config = require("editor.comment"),
 }
+-- git做文件比较（diff）
 editor["sindrets/diffview.nvim"] = {
 	lazy = true,
 	cmd = { "DiffviewOpen", "DiffviewClose" },
 }
-editor["junegunn/vim-easy-align"] = {
-	lazy = true,
-	cmd = "EasyAlign",
-}
+-- 对其插件
+-- editor["junegunn/vim-easy-align"] = {
+-- 	lazy = true,
+-- 	cmd = "EasyAlign",
+-- }
+-- 快速跳转
 editor["phaazon/hop.nvim"] = {
 	lazy = true,
 	branch = "v2",
 	event = "BufReadPost",
 	config = require("editor.hop"),
 }
-editor["RRethy/vim-illuminate"] = {
-	lazy = true,
-	event = { "CursorHold", "CursorHoldI" },
-	config = require("editor.vim-illuminate"),
-}
+-- 不知道有啥用
+-- editor["RRethy/vim-illuminate"] = {
+-- 	lazy = true,
+-- 	event = { "CursorHold", "CursorHoldI" },
+-- 	config = require("editor.vim-illuminate"),
+-- }
 editor["luukvbaal/stabilize.nvim"] = {
 	lazy = true,
 	event = "BufReadPost",
